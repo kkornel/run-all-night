b5d0a507844b8a3ae72c9745c96402c38d546c0c
Kornel <krzeslak.kornel@gmail.com>
2018-10-27 10:41:35 +0200

    Changed ids.

--- app/src/main/java/com/example/kornel/alphaui/MainActivity.java
+++ app/src/main/java/com/example/kornel/alphaui/MainActivity.java
@@ -27,10 +27,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+        // implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
 
@@ -38,11 +41,30 @@ public class MainActivity extends AppCompatActivity
 
     private final int PICK_ACTIVITY_REQUEST = 1;
 
-    private Button mStartActivityButton;
-    private CardView mMusicCardView;
+    // Welcome CardView
+    private TextView mWelcomeTextView;
+    private TextView mLastTrainingTextView;
 
+    // Weather CardView
+    private CardView mWeatherCardView;
+    private ImageView mWeatherImageView;
+    private TextView mWeatherTextView;
+    private TextView mWeatherInfoTextView;
+    private TextView mTempTextView;
+
+    // Activity CardView
     private CardView mActivityCardView;
+    private ImageView mActivityImageView;
     private TextView mActivityNameTextView;
+
+    // Music CardView
+    private CardView mMusicCardView;
+    private ImageView mMusicImageView;
+    private TextView mSelectMusicTextView;
+
+    private Button mStartActivityButton;
+
+    private BottomNavigationView mBottomNavigationView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -67,19 +89,47 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        mWelcomeTextView = findViewById(R.id.welcomeTextView);
+        mLastTrainingTextView = findViewById(R.id.lastTrainingTextView);
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mWeatherCardView = findViewById(R.id.weatherCardView);
+        mWeatherImageView = findViewById(R.id.weatherImageView);
+        mWeatherTextView = findViewById(R.id.weatherTextView);
+        mWeatherInfoTextView = findViewById(R.id.weatherInfoTextView);
+        mTempTextView = findViewById(R.id.tempTextView);
+
+        mActivityCardView = findViewById(R.id.activityCardView);
+        mActivityCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChooseSportActivity.class);
+                startActivityForResult(intent, PICK_ACTIVITY_REQUEST);
+            }
+        });
+        mActivityImageView = findViewById(R.id.activityImageView);
+        mActivityNameTextView = findViewById(R.id.activityNameTextView);
+
+        mMusicCardView = findViewById(R.id.musicCardView);
+        mMusicCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_MUSIC);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        mMusicImageView = findViewById(R.id.musicImageView);
+        mSelectMusicTextView = findViewById(R.id.selectMusicTextView);
+
+        mWeatherCardView = findViewById(R.id.weatherCardView);
 
         mStartActivityButton = findViewById(R.id.startActivityButton);
         mStartActivityButton.setOnClickListener(new View.OnClickListener() {
@@ -90,32 +140,11 @@ public class MainActivity extends AppCompatActivity
                 Log.d(TAG, "isInternetConnection: " + isInternetConnection());
             }
         });
-        
-        mMusicCardView = findViewById(R.id.musicCV);
-        mMusicCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_APP_MUSIC);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
 
-            }
-        });
-        mActivityCardView = findViewById(R.id.activityCV);
-        mActivityCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ChooseSportActivity.class);
-                startActivityForResult(intent, PICK_ACTIVITY_REQUEST);
-            }
-        });
+        mBottomNavigationView = findViewById(R.id.navigation);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mBottomNavigationView.setSelectedItemId(R.id.navigation_dashboard);
 
-        mActivityNameTextView = findViewById(R.id.activityNameTextView);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
@@ -169,60 +198,75 @@ public class MainActivity extends AppCompatActivity
         return isConnected;
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+    // @Override
+    // public void onBackPressed() {
+    //     DrawerLayout drawer = findViewById(R.id.drawer_layout);
+    //     if (drawer.isDrawerOpen(GravityCompat.START)) {
+    //         drawer.closeDrawer(GravityCompat.START);
+    //     } else {
+    //         super.onBackPressed();
+    //     }
+    // }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    // @Override
+    // public boolean onCreateOptionsMenu(Menu menu) {
+    //     // Inflate the menu; this adds items to the action bar if it is present.
+    //     getMenuInflater().inflate(R.menu.main, menu);
+    //     return true;
+    // }
+    //
+    // @Override
+    // public boolean onOptionsItemSelected(MenuItem item) {
+    //     // Handle action bar item clicks here. The action bar will
+    //     // automatically handle clicks on the Home/Up button, so long
+    //     // as you specify a parent activity in AndroidManifest.xml.
+    //     int id = item.getItemId();
+    //
+    //     //noinspection SimplifiableIfStatement
+    //     if (id == R.id.action_settings) {
+    //         return true;
+    //     }
+    //
+    //     return super.onOptionsItemSelected(item);
+    // }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    // @SuppressWarnings("StatementWithEmptyBody")
+    // @Override
+    // public boolean onNavigationItemSelected(MenuItem item) {
+    //     // Handle navigation view item clicks here.
+    //     int id = item.getItemId();
+    //
+    //     if (id == R.id.nav_camera) {
+    //         // Handle the camera action
+    //     } else if (id == R.id.nav_gallery) {
+    //
+    //     } else if (id == R.id.nav_slideshow) {
+    //
+    //     } else if (id == R.id.nav_manage) {
+    //
+    //     } else if (id == R.id.nav_share) {
+    //
+    //     } else if (id == R.id.nav_send) {
+    //
+    //     }
+    //
+    //     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    //     drawer.closeDrawer(GravityCompat.START);
+    //     return true;
+    // }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
 
-        return super.onOptionsItemSelected(item);
-    }
+        // DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        // ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        //         this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        // drawer.addDrawerListener(toggle);
+        // toggle.syncState();
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        // NavigationView navigationView = findViewById(R.id.nav_view);
+        // navigationView.setNavigationItemSelectedListener(this);
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+    // }
 }
