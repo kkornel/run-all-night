d64bbadba77c4af3bddf830728a9bb5699145401
Kornel <krzeslak.kornel@gmail.com>
2018-10-28 06:57:31 +0100

    Swiping to fragments

--- app/src/main/java/com/example/kornel/alphaui/MainActivity.java
+++ app/src/main/java/com/example/kornel/alphaui/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
