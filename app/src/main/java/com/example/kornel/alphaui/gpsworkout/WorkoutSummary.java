5ccd5d13067e0d20e08038890f1ca500939bee19
Kornel <krzeslak.kornel@gmail.com>
2018-11-20 08:56:41 +0100

    Added gallery intent

--- app/src/main/java/com/example/kornel/alphaui/gpsworkout/WorkoutSummary.java
+++ app/src/main/java/com/example/kornel/alphaui/gpsworkout/WorkoutSummary.java
@@ -1,61 +1,163 @@
 package com.example.kornel.alphaui.gpsworkout;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.kornel.alphaui.R;
-import com.example.kornel.alphaui.mainactivity.MainActivity;
 import com.example.kornel.alphaui.utils.Database;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.text.DateFormat;
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.Date;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 import static com.example.kornel.alphaui.gpsworkout.StartGpsWorkoutActivity.WORKOUT_DETAILS_EXTRA_INTENT;
 
-public class WorkoutSummary extends AppCompatActivity {
+public class WorkoutSummary extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = "WorkoutSummary";
 
-    private TextView mTimeTextView;
+    private static final int PICK_IMAGE_REQUEST = 123;
+
+    private CardView mWorkoutCardView;
+    private ImageView mActivityIconImageView;
+    private TextView mActivityTypeTextView;
+    private TextView mDateTextView;
+
+    private GoogleMap mMap;
+
+    private CardView mSummaryCardView;
+    private ImageView mDurationImageView;
+    private TextView mDurationTextView;
+    private ImageView mAvgPaceImageView;
+    private TextView mAvgPaceTextView;
+    private ImageView mAvgSpeedImageView;
+    private TextView mAvgSpeedTextView;
+    private ImageView mDistanceImageView;
     private TextView mDistanceTextView;
+    private ImageView mMaxPaceImageView;
+    private TextView mMaxPaceTextView;
+    private ImageView mMaxSpeedImageView;
+    private TextView mMaxSpeedTextView;
+
+    private CardView mStatusCardView;
+    private EditText mStatusEditText;
+
+    private CardView mPhotoCardView;
+    private ImageView mPhotoIconImageView;
+    private Button mBrowseButton;
+    private CardView mSelectedPhotoCardView;
+    private ImageView mSelectedImageImageVIew;
+    private Button mDeletePhotoButton;
+
+    private CardView mPrivacyCardView;
+    private Spinner mPrivacySettingsSpinner;
+
+    private CardView mWeatherCardView;
+    private TextView mWeatherSummaryTextView;
+    private ImageView mWeatherImageView;
+    private TextView mWeatherTempTextView;
+
+    private CardView mLapsCardView;
+
     private Button mSaveButton;
-    private Button mDontSaveButton;
+    private Button mDeleteButton;
+
+    private AlertDialog mCameraGalleryDialog;
+    private Bitmap mSelectedPhotoBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate: ");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_summary);
 
-        // mTimeTextView = findViewById(R.id.timetv);
-        // mDistanceTextView = findViewById(R.id.distancetv);
-        // mSaveButton = findViewById(R.id.saveButton);
-        // mDontSaveButton = findViewById(R.id.dontSaveButton);
+        getSupportActionBar().setTitle("Podsumowanie");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        // final WorkoutGpsSummary workoutSummary = (WorkoutGpsSummary) getIntent().getExtras().getParcelable(WORKOUT_DETAILS_EXTRA_INTENT);
+        mWorkoutCardView = findViewById(R.id.workoutCardView);
+        mActivityIconImageView = findViewById(R.id.activityIconImageView);
+        mActivityTypeTextView = findViewById(R.id.activityTypeTextView);
+        mDateTextView = findViewById(R.id.dateTextView);
+
+        SupportMapFragment map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.summaryMap));
+        map.getMapAsync(this);
+
+        mSummaryCardView = findViewById(R.id.summaryCardView);
+        mDurationImageView = findViewById(R.id.durationImageView);
+        mDurationTextView = findViewById(R.id.durationTextView);
+        mAvgPaceImageView = findViewById(R.id.avgPaceImageView);
+        mAvgPaceTextView = findViewById(R.id.avgPaceTextView);
+        mAvgSpeedImageView = findViewById(R.id.avgSpeedImageView);
+        mAvgSpeedTextView = findViewById(R.id.avgSpeedTextView);
+        mDistanceImageView = findViewById(R.id.distanceImageView);
+        mDistanceTextView = findViewById(R.id.distanceTextView);
+        mMaxPaceImageView = findViewById(R.id.maxPaceImageView);
+        mMaxPaceTextView = findViewById(R.id.maxPaceTextView);
+        mMaxSpeedImageView = findViewById(R.id.maxSpeedImageView);
+        mMaxSpeedTextView = findViewById(R.id.maxSpeedTextView);
+
+        mStatusCardView = findViewById(R.id.statusCardView);
+        mStatusEditText = findViewById(R.id.statusEditText);
+
+        mPhotoCardView = findViewById(R.id.photoCardView);
+        mPhotoIconImageView = findViewById(R.id.photoIconImageView);
+        mBrowseButton = findViewById(R.id.browseButton);
+        mBrowseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCameraGalleryDialog();
+            }
+        });
+        mSelectedPhotoCardView = findViewById(R.id.selectedPhotoCardView);
+        hideSelectedPhotoCardView();
+        mSelectedImageImageVIew = findViewById(R.id.selectedImageImageView);
+        mDeletePhotoButton = findViewById(R.id.deletePhotoButton);
+        mDeletePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteSelectedPhoto();
+            }
+        });
+
+        mPrivacyCardView = findViewById(R.id.privacyCardView);
+        mPrivacySettingsSpinner = findViewById(R.id.privacySettingsSpinner);
+
+        mWeatherCardView = findViewById(R.id.weatherCardView);
+        mWeatherSummaryTextView = findViewById(R.id.weatherSummaryTextView);
+        mWeatherImageView = findViewById(R.id.weatherImageView);
+        mWeatherTempTextView = findViewById(R.id.weatherTempTextView);
+
+        mLapsCardView = findViewById(R.id.lapsCardView);
+
+        mSaveButton = findViewById(R.id.saveButton);
+        mDeleteButton = findViewById(R.id.deleteButton);
+
+        final WorkoutGpsSummary workoutSummary = getIntent().getExtras().getParcelable(WORKOUT_DETAILS_EXTRA_INTENT);
         //
         // mTimeTextView.setText(workoutSummary.getDuration());
         //
@@ -94,9 +196,97 @@ public class WorkoutSummary extends AppCompatActivity {
         // });
     }
 
+    private void deleteSelectedPhoto() {
+        mSelectedPhotoBitmap = null;
+        mSelectedImageImageVIew.setImageBitmap(mSelectedPhotoBitmap);
+        hideSelectedPhotoCardView();
+    }
+
+    private void showCameraGalleryDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Get the layout inflater
+        final LayoutInflater inflater = getLayoutInflater();
+
+        View vView = inflater.inflate(R.layout.select_photo_dialog, null);
+        final Button cameraButton = vView.findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(WorkoutSummary.this, "camera", Toast.LENGTH_SHORT).show();
+            }
+        });
+        final Button galleryButton = vView.findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                browseForImage();
+            }
+        });
+
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(vView).setTitle("Wybierz akcje:");
+
+        mCameraGalleryDialog = builder.create();
+        mCameraGalleryDialog.show();
+
+    }
+
+    private void browseForImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.edit_profile_chooser_title)), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        mCameraGalleryDialog.dismiss();
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+
+            showSelectedPhotoCardView();
+
+            try {
+                mSelectedPhotoBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                mSelectedImageImageVIew.setImageBitmap(mSelectedPhotoBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            mSelectedPhotoBitmap = null;
+            hideSelectedPhotoCardView();
+        }
+    }
+
+    private void hideSelectedPhotoCardView() {
+        mSelectedPhotoCardView.setVisibility(View.GONE);
+    }
+
+    private void showSelectedPhotoCardView() {
+        mSelectedPhotoCardView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
     public void requestInternetConnection() {
         Snackbar.make(
-                mSaveButton,
+                mSummaryCardView,
                 R.string.enable_internet,
                 Snackbar.LENGTH_INDEFINITE)
                 .setAction(R.string.settings, new View.OnClickListener() {
@@ -107,6 +297,28 @@ public class WorkoutSummary extends AppCompatActivity {
                     }
                 })
                 .show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_save_delete_workout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.save_workout:
+                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete_workout:
+                Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void saveWorkout(WorkoutGpsSummary workoutSummary) {
@@ -134,12 +346,4 @@ public class WorkoutSummary extends AppCompatActivity {
         database.getReference().updateChildren(childUpdates);
     }
 
-    private boolean hasInternetConnection() {
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
-        return isConnected;
-    }
 }
