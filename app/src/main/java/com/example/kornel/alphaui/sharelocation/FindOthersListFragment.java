d2a414fcbea40378e45afa5972b29e0eeb44ebd0
Kornel <krzeslak.kornel@gmail.com>
2019-01-09 16:31:11 +0100

    Dialog in the shared list

--- app/src/main/java/com/example/kornel/alphaui/sharelocation/FindOthersListFragment.java
+++ app/src/main/java/com/example/kornel/alphaui/sharelocation/FindOthersListFragment.java
@@ -1,6 +1,7 @@
 package com.example.kornel.alphaui.sharelocation;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -22,11 +23,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.kornel.alphaui.R;
+import com.example.kornel.alphaui.ViewProfileActivity;
+import com.example.kornel.alphaui.profile.EditProfileActivity;
+import com.example.kornel.alphaui.profile.ProfileDetailsActivity;
 import com.example.kornel.alphaui.utils.Database;
 import com.example.kornel.alphaui.utils.ListItemClickListener;
 import com.example.kornel.alphaui.utils.User;
 import com.example.kornel.alphaui.utils.Utils;
 import com.example.kornel.alphaui.weather.LocationUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -38,6 +46,8 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.kornel.alphaui.sharelocation.FindOthersMapFragment.EXTRA_USER;
 
 public class FindOthersListFragment extends Fragment implements ListItemClickListener, LocationUtils.MyLocationResult {
     private static final String TAG = "FindOthersListFragment";
@@ -66,7 +76,6 @@ public class FindOthersListFragment extends Fragment implements ListItemClickLis
     private DatabaseReference mUsersRef;
     private DatabaseReference mFriendRequestsRef;
     private String mUserUid;
-    private String mFriendUid;
 
     private boolean mFirstLocationUpdate = true;
 
@@ -177,7 +186,7 @@ public class FindOthersListFragment extends Fragment implements ListItemClickLis
                 throw databaseError.toException();
             }
         };
-        userFriendRef.addListenerForSingleValueEvent(friendsListener);
+        //userFriendRef.addListenerForSingleValueEvent(friendsListener);
 
         nameTextView.setText(sli.getUserProfile().getFullName());
 
@@ -191,14 +200,106 @@ public class FindOthersListFragment extends Fragment implements ListItemClickLis
         mInfoWindowDialog = builder.create();
         mInfoWindowDialog.show();
     }
+    public void showConfirmPasswordDialog(int clickedItemIndex) {
+        final SharedLocationInfo sli = mSharedLocationInfoList.get(clickedItemIndex);
+        final String userUid = mSharedLocationInfoList.get(clickedItemIndex).getUserUid();
 
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        // Get the layout inflater
+        final LayoutInflater inflater = getLayoutInflater();
+
+        View vView = inflater.inflate(R.layout.marker_info_window, null);
+        ImageView avatarImageView = vView.findViewById(R.id.avatarImageView);
+        TextView nameTextView = vView.findViewById(R.id.nameTextView);
+        TextView distanceLabel = vView.findViewById(R.id.distanceLabel);
+        TextView distanceTextView = vView.findViewById(R.id.distanceTextView);
+        TextView messageTextView = vView.findViewById(R.id.messageTextView);
+        Button showProfileButton = vView.findViewById(R.id.viewProfileButton);
+        showProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), ViewProfileActivity.class);
+                i.putExtra(EXTRA_USER, sli.getUserProfile());
+
+                startActivity(i);
+            }
+        });
+        final Button addToFriendsButton = vView.findViewById(R.id.addToFriendButton);
+        addToFriendsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFriendRequestsRef.child(mUserUid).child(userUid).setValue(Database.FRIENDS_REQUESTS_SENT);
+                mFriendRequestsRef.child(userUid).child(mUserUid).setValue(Database.FRIENDS_REQUESTS_RECEIVED);
+
+                Toast.makeText(getContext(), R.string.friends_dialog_invitation_sent, Toast.LENGTH_SHORT).show();
+
+                addToFriendsButton.setText(getString(R.string.friends_dialog_invitation_sent));
+                addToFriendsButton.setEnabled(false);
+            }
+        });
+
+        DatabaseReference userFriendRef = mUsersRef.child(mUserUid).child(Database.FRIENDS).child(userUid);
+        ValueEventListener friendsListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null) {
+                    addToFriendsButton.setEnabled(false);
+                    addToFriendsButton.setText(getString(R.string.already_a_friends));
+                } else {
+
+                    DatabaseReference userFriendRequestsRef = mFriendRequestsRef.child(mUserUid).child(userUid);
+                    ValueEventListener userFriendRequestsListener = new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.getValue() != null) {
+                                addToFriendsButton.setEnabled(false);
+                                addToFriendsButton.setText(getString(R.string.friends_dialog_invitation_sent));
+                            } else {
+                                addToFriendsButton.setEnabled(true);
+                                addToFriendsButton.setText(getString(R.string.invite_to_friends));
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Log.e(TAG, "onCancelled: " + databaseError.getMessage());
+                            throw databaseError.toException();
+                        }
+                    };
+                    userFriendRequestsRef.addListenerForSingleValueEvent(userFriendRequestsListener);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled: " + databaseError.getMessage());
+                throw databaseError.toException();
+            }
+        };
+        userFriendRef.addListenerForSingleValueEvent(friendsListener);
+
+        nameTextView.setText(sli.getUserProfile().getFullName());
+
+        distanceTextView.setText(sli.getDistanceToYouString() + "km");
+        messageTextView.setText(sli.getMessage());
+
+        Picasso.get()
+                .load(sli.getUserProfile().getAvatarUrl())
+                .into(avatarImageView);
+
+        final AlertDialog dialog;
+
+        builder.setView(vView);
+        dialog = builder.create();
+        dialog.show();
+    }
     @Override
     public void onListItemClick(int clickedItemIndex) {
         String userUid = mSharedLocationInfoList.get(clickedItemIndex).getUserUid();
 
 
-
-        showCameraGalleryDialog(clickedItemIndex);
+        showConfirmPasswordDialog(clickedItemIndex);
+        // showCameraGalleryDialog(clickedItemIndex);
 
 
 
